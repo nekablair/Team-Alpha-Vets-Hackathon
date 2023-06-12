3f82c7ba7535b380ff4cc135829c45669f876dfa
GP <gregpetropoulos@yahoo.com>
2023-06-11 22:41:44 -0400

    Installed the Cypress testing suite with a basic Header and Footer Component test and an End to End test on the application mounting on a visit to home url.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Container } from 'react-bootstrap';
-import Header from './components/Header';
-import Footer from './components/Footer';
+import Header from './components/Header/Header';
+import Footer from './components/Footer/Footer';
 import HomeScreen from './screens/HomeScreen';
 // TODO IMPLEMENT REACT ROUTER
 
