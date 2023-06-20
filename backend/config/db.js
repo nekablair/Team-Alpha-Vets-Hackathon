f8bdd6f07f9bed5bfbfc96b1f6446460326812bd
GP <gregpetropoulos@yahoo.com>
2023-06-20 09:30:04 -0400

    Created a connection to mongodb for database access

---
+++ backend/config/db.js
@@ -0,0 +1,12 @@
+import mongoose from 'mongoose';
+const connectDB = async () => {
+  try {
+    mongoose.set('strictQuery', false);
+    const conn = await mongoose.connect(process.env.MONGO_URI);
+    console.log(`Mongo DB Connected ${conn.connection.host}`.cyan.underline);
+  } catch (err) {
+    console.error(`DB Connection Error: ${err.message}`.red.underline.bold);
+    process.exit(1);
+  }
+};
+export default connectDB;
