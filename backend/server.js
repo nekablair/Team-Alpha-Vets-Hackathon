9d51bb4ccde5331852daa093fe27a14a8d65a42e
GP <gregpetropoulos@yahoo.com>
2023-06-22 16:57:33 -0400

    Abstracted the api routes from the server file

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,7 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import axios from 'axios';
+import vaRoutes from './routes/vaRoutes.js';
 import connectDB from './config/db.js';
 import colors from 'colors';
 dotenv.config();
@@ -7,8 +9,16 @@ connectDB();
 
 const PORT = process.env.PORT || 5000;
 const app = express();
-app.get('/', (req, res) => {
-  res.send('API IS RUNNING 200 OK');
-});
+
+//TEST ROUTE
+// app.get('/', (req, res) => {
+//   res.send('API IS RUNNING 200 OK');
+// });
+
+//for body parsing the req.body in routes&controllers
+app.use(express.json());
+
+//ROUTES
+app.use('/api/va', vaRoutes);
 
 app.listen(PORT, () => console.log(`Server is running on ${PORT}`));
