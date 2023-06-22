1fd241d937d0fc2a3d13a1316b08819b9d02d7c3
GP <gregpetropoulos@yahoo.com>
2023-06-22 17:29:43 -0400

    Bootstrap styles were missing from previous merge not set to develop branch. I added the custom bootstrap styles back

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,8 +1,12 @@
+
+
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-// import 'bootstrap/dist/css/bootstrap.min.css'//!default styles if custom not used
-import './assets/styles/bootstrap.custom.css';
-import './assets/styles/index.css';
+//import 'bootstrap/dist/css/bootstrap.min.css'//!default styles if custom not used
+// import './assets/styles/bootstrap.custom.css';//!custom artifacts
+// import './assets/styles/index.css';//!custom artifacts
+import '../node_modules/bootstrap/dist/css/bootstrap.css';
+import '../src/scss/custom.scss';
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
@@ -11,3 +15,18 @@ root.render(
     <App />
   </React.StrictMode>
 );
+
+
+// import React from 'react';
+// import ReactDOM from 'react-dom/client';
+// // import 'bootstrap/dist/css/bootstrap.min.css'//!default styles if custom not used
+// import './assets/styles/bootstrap.custom.css';
+// import './assets/styles/index.css';
+// import App from './App';
+
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+// root.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>
+// );
