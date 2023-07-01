ee653fddee1632dd881843ee7290a4e38dc04e24
Michael J Sigg <mjsigg@gmail.com>
2023-07-01 02:53:12 -0500

    Made a successful API call and grabbed the params

--- frontend/src/screens/FormsScreen.jsx
+++ frontend/src/screens/FormsScreen.jsx
@@ -1,38 +1,100 @@
-import React from 'react';
-import {Row, Container, Col} from 'react-bootstrap';
+import React, { useState } from 'react';
+import { Row, Container, Col } from 'react-bootstrap';
+import axios from 'axios';
 
 const FormsScreen = () => {
+  const [city, setCity] = useState('');
+  const [state, setState] = useState('');
+  const [zip, setZip] = useState('');
+  const [results, setResults] = useState([]);
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    // Make a GET request to the endpoint using Axios
+    axios
+      .get('https://sandbox-api.va.gov/services/va_facilities/v0/facilities/all', {
+        headers: {
+          apikey: 'gc6FNZ9x6hV2VPEIwgBZLklGAwJHzg37',
+          'Content-Type': 'application/json',
+        },
+        params: {
+          city: city,
+          state: state,
+          zip: zip,
+        },
+      })
+      .then((response) => {
+        const features = response.data.features;
+        const cities = [];
+
+        for (let i = 0; i < features.length; i++) {
+          const feature = features[i];
+          const featureCity = feature.properties.address.physical.city;
+
+          if (city === featureCity) {
+            cities.push(featureCity);
+          }
+        }
+
+        setResults(cities);
+      })
+      .catch((error) => {
+        console.error('Error:', error);
+      });
+  };
 
   return (
     <Container>
       <Row>
         <Col>
-        <div className='mb-3'>
+          <div className='mb-3'>
             <h1>FormsScreen</h1>
 
-            <form action="POST">
+            <form onSubmit={handleSubmit}>
+              <label htmlFor='city'>City</label>
+              <input
+                type='text'
+                id='city'
+                placeholder='Enter city ...'
+                value={city}
+                onChange={(e) => setCity(e.target.value)}
+              />
 
-              {/* <label htmlFor="firstName">First Name</label>
-              <input type="text" id='firstName' placeholder='Enter first name ...'/>
+              <label htmlFor='state'>State</label>
+              <input
+                type='text'
+                id='state'
+                placeholder='Enter state ...'
+                value={state}
+                onChange={(e) => setState(e.target.value)}
+              />
 
-              <label htmlFor="lastName">Last Name</label>
-              <input type="text" id='lastName' placeholder='Enter last name ...'/>
+              <label htmlFor='zip'>Zip</label>
+              <input
+                type='text'
+                id='zip'
+                placeholder='Enter zip ...'
+                value={zip}
+                onChange={(e) => setZip(e.target.value)}
+              />
 
-              <label htmlFor="address">Address</label>
-              <input type="text" id='address' placeholder='Enter address ...'/> */}
-
-              <label htmlFor="city">City</label>
-              <input type="text" id='city' placeholder='Enter city ...'/>
-
-              <label htmlFor="state">State</label>
-              <input type="text" id='state' placeholder='Enter state ...' />
-
-              <label htmlFor="zip">Zip</label>
-              <input type="zip"  placeholder='Enter zip ...'/>
-
+              <button type='submit'>Submit</button>
             </form>
 
-        </div>
+            <div>
+              <h2>Results:</h2>
+              {results.length > 0 ? (
+                <ul>
+                  {results.map((city, index) => (
+                    <li key={index}>{city}</li>
+                  ))}
+                </ul>
+              ) : (
+                <p>No matching cities found.</p>
+              )}
+            </div>
+          </div>
         </Col>
       </Row>
     </Container>
