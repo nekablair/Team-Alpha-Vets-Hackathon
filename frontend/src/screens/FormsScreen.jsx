b8a936fab063ad960314c92783701b42e019f0f1
Michael J Sigg <mjsigg@gmail.com>
2023-06-30 23:40:50 -0500

    Forms Screen First Draft

--- frontend/src/screens/FormsScreen.jsx
+++ frontend/src/screens/FormsScreen.jsx
@@ -1,7 +1,42 @@
 import React from 'react';
+import {Row, Container, Col} from 'react-bootstrap';
 
 const FormsScreen = () => {
-  return <h1>FormsScreen</h1>;
+
+  return (
+    <Container>
+      <Row>
+        <Col>
+        <div className='mb-3'>
+            <h1>FormsScreen</h1>
+
+            <form action="POST">
+
+              {/* <label htmlFor="firstName">First Name</label>
+              <input type="text" id='firstName' placeholder='Enter first name ...'/>
+
+              <label htmlFor="lastName">Last Name</label>
+              <input type="text" id='lastName' placeholder='Enter last name ...'/>
+
+              <label htmlFor="address">Address</label>
+              <input type="text" id='address' placeholder='Enter address ...'/> */}
+
+              <label htmlFor="city">City</label>
+              <input type="text" id='city' placeholder='Enter city ...'/>
+
+              <label htmlFor="state">State</label>
+              <input type="text" id='state' placeholder='Enter state ...' />
+
+              <label htmlFor="zip">Zip</label>
+              <input type="zip"  placeholder='Enter zip ...'/>
+
+            </form>
+
+        </div>
+        </Col>
+      </Row>
+    </Container>
+  );
 };
 
 export default FormsScreen;
