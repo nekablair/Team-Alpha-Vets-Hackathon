ac3745b6f9da53bb16f5e7244a9020b2aecd9e6c
GP <gregpetropoulos@yahoo.com>
2023-07-04 00:05:25 -0400

    Refactor footer

--- frontend/src/components/Footer/Footer.jsx
+++ frontend/src/components/Footer/Footer.jsx
@@ -1,21 +1,15 @@
 import React from 'react';
 import { Container, Row, Col } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
-import footerImg from '../images/footer.png';
 
 const Footer = () => {
   const currentYear = new Date().getFullYear();
 
   return (
-    <footer className='text-center'>
-      <Container
-        className='d-flex flex-column align-items-center justify-content-center p-4 object-fit-cover '
-        style={{
-          backgroundImage: `url(${footerImg})`,
-        }}
-      >
-        <Row>
-          <Row>
+    <footer className=' footer-background-image'>
+      <Container>
+        <Row className='justify-content-center'>
+          <Col className='align-items-center'>
             <h3>Equine Therapy</h3>
             <Link to='/about'>
               <h6>About</h6>
@@ -26,16 +20,18 @@ const Footer = () => {
             <Link to='/contact'>
               <h6>Contact Us</h6>
             </Link>
+          </Col>
+          <Row className='justify-content-between footer-gradient'>
+            <Col>
+              <p className='text-black-fade'>Equine Therapy App &copy; {currentYear}</p>
+            </Col>
+            <Col>
+              <p>Privacy Policy</p>
+            </Col>
+            <Col>
+              <p>Made by Team Alpha</p>
+            </Col>
           </Row>
-          <Col>
-            <p className='text-black-fade'>Equine Therapy App &copy; {currentYear}</p>
-          </Col>
-          <Col>
-            <p>Privacy Policy</p>
-          </Col>
-          <Col>
-            <p>Made by Team Alpha</p>
-          </Col>
         </Row>
       </Container>
     </footer>
