8ea4ec2d8cff1b97dc91888322078f9af49d1411
GP <gregpetropoulos@yahoo.com>
2023-07-03 23:40:22 -0400

    Refactored

--- frontend/src/components/Hero/Hero.jsx
+++ frontend/src/components/Hero/Hero.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
-import { Button } from 'react-bootstrap';
-import backgroundImage from '../images/hero-stock-1.png';
+import { Button, Row,Container } from 'react-bootstrap';
 
 const Hero = () => {
   const navigate = useNavigate();
@@ -11,27 +10,15 @@ const Hero = () => {
   };
 
   return (
-    <div className='text-center'>
-      <div
-        className='d-flex flex-column align-items-center justify-content-center h-75  p-5 object-fit-contain center-block'
-        style={{
-          backgroundImage: `url(${backgroundImage})`,
-        }}
-      >
-        <div
-          className='mask py-4'
-          style={{
-            backgroundColor: 'rgba(0, 0, 0, 0.6)',
-          }}
-        >
-          <h1 className='text-white'>Equine Therapy Healing for Veterans</h1>
-          <p className='text-white text-uppercase'>Learn more about equine/horse therapy</p>
-          <Button variant='btn btn-outline-secondary' onClick={goToPrograms}>
-            Learn More
-          </Button>
-        </div>
-      </div>
-    </div>
+        <Container fluid className='justify-content-center image-overlay'>
+          <Row className='image-overlay-content'>
+            <h1 className='text-white'>Equine Therapy Healing for Veterans</h1>
+            <p className='text-white text-uppercase'>Learn more about equine/horse therapy</p>
+            <Button variant='btn btn-outline-secondary w-50' onClick={goToPrograms}>
+              Learn More
+            </Button>
+          </Row>
+        </Container>
   );
 };
 
